b79d5ba4c90c760906e91d27ef73d11bf01509b4
rajat <rajatkantinandi1993@gmail.com>
2018-02-18 12:49:07 +0530

    created header-parser

---
+++ server.js
@@ -0,0 +1,27 @@
+var express=require('express');
+var useragent=require('express-useragent');
+var path=require('path');
+var app=express();
+app.use(useragent.express());
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname,"views","index.html"));
+});
+app.get('/style.css',function(req,res){
+    res.sendFile(path.join(__dirname,"views","style.css"));
+});
+app.get('/api/whoami',function(req,res){
+    var ipAddress=req.ip;
+    var langL=req.acceptsLanguages()[0];
+    var software={
+        "OS":req.useragent.os,
+        "Browser":req.useragent.browser
+    };
+    res.send({
+        ipaddress:ipAddress,
+        language:langL,
+        software:software
+    });
+});
+app.listen(process.argv[3],process.argv[2],function(){
+    console.log("listening to request on: //"+process.argv[2]+":"+process.argv[3]);
+});
